4db312ac8dff0e93da07c3e19cf2f0c32a932a2a
zmgu <122958434+zmgu@users.noreply.github.com>
2023-08-21 22:09:01 +0900

    Deprecated 수정

--- src/main/java/com/study/security/configuration/SecurityConfig.java
+++ src/main/java/com/study/security/configuration/SecurityConfig.java
@@ -7,10 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,19 +25,14 @@ public class SecurityConfig {
     private final UserService userService;
     @Value("${security.jwt.token.secret}")
     private String secretKey;
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/예외처리하고 싶은 url", "/예외처리하고 싶은 url");
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
+        http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
                         .requestMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
-                        .requestMatchers("/api/v1/**").authenticated()
-                        .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/**").authenticated()
                 ).sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class);
--- src/main/java/com/study/security/utils/JwtUtil.java
+++ src/main/java/com/study/security/utils/JwtUtil.java
@@ -5,27 +5,31 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-
 import java.security.Key;
 import java.util.Date;
 
 public class JwtUtil {
 
-    public  static String getUserName(String token, String key) {
+    public  static String getUserName(String token, String secret) {
+
+        Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)
                 .getBody().get("userName", String.class);
     }
 
-    public static boolean isExpired(String token, String secretKey) {
-        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token)
+    public static boolean isExpired(String token, String secret) {
+
+        Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)
                 .getBody().getExpiration().before(new Date());
     }
     public static String createToken(String userName, String secret, Long expireTimeMs) {
         Claims claims = Jwts.claims();
         claims.put("userName", userName);
 
-        byte[] keyBytes = Decoders.BASE64.decode(secret);
-        Key key = Keys.hmacShaKeyFor(keyBytes);
+        Key key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
 
         return Jwts.builder()
                 .setClaims(claims)
